3013dc44600d3d8f6717638476b9234c4f962a13
zoe <zoe2002klein@gmail.com>
2026-01-29 01:15:53 +0100

    Add JavaScriptTriviaBot with coding fun facts

---
+++ JavaScript Certificate/JavaScriptTriviaBot.js
@@ -0,0 +1,24 @@
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "coolBot";
+let botLocation = "the Moon";
+let favoriteLanguage = "JS";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Did you know that " + favoriteLanguage + " was created in just 10 days? That's crazy right.";
+
+console.log(codingFact);
+
+codingFact = "Did you know that " + favoriteLanguage + "  was probably named after Java";
+
+console.log(codingFact);
+
+codingFact = "GitHub says " + favoriteLanguage + " is the Most Popular language in the world.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
