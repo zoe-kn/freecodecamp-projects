8125d874074749a1fec921a8cb2f5998060f0bac
zoe <zoe2002klein@gmail.com>
2026-01-29 14:50:50 +0100

    Create string_transformer.js

---
+++ JavaScript Certificate/string_transformer.js
@@ -0,0 +1,26 @@
+const originalString = "I love cats.";
+console.log("Original string:");
+console.log(originalString);
+
+const replacedString = originalString.replace("cats", "dogs");
+console.log("After using the replace() method:");
+console.log(replacedString);
+
+const exampleSentence = "I love cats and cats are so much fun!";
+console.log("Original sentence:");
+console.log(exampleSentence);
+
+const dogsOnlySentence = exampleSentence.replaceAll("cats", "dogs");
+console.log("Replacing all occurrences of cats with dogs:");
+console.log(dogsOnlySentence);
+
+const learningSentence = "I love learning!";
+console.log("Original learning sentence:");
+console.log(learningSentence);
+
+const repeatedLove = "love ".repeat(3).trimEnd();
+console.log(repeatedLove);
+
+const newSentence = `I ${repeatedLove} learning.`;
+console.log(newSentence);
+
